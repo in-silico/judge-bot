2f98f710062ff6d714bc22721c925ccb1e7f464b
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-12 09:28:08 -0500

    Setting working directory

--- judge.sh
+++ judge.sh
@@ -5,14 +5,14 @@ compilation="/usr/bin/g++ -o2 -static -pipe -o source source.cpp"
 execution="./source < main.in > main.out"
 host_dir="`pwd`""/run"$sub_id
 container_dir="/tmp/run"$sub_id
-commands="cd $container_dir && $compilation && $execution"
+commands="$compilation && $execution"
 
 max_run_time="10"
 max_comp_time="10"
 tl="6"
 ml="250"
 
-container_id=$(docker run -d -v $host_dir:$container_dir debian-testing sh -c "$commands")
+container_id=$(docker run -w $container_dir -d -v $host_dir:$container_dir debian-testing sh -c "$commands")
 exit_code=$(timeout "$max_run_time" docker wait "$container_id" || true)
 running=$(docker inspect --format="{{ .State.Running }}" "$container_id" 2> /dev/null)
 
