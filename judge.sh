e49066f0fda77393f350d6ea200945370f69526b
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-11 19:03:40 -0500

    Added time out for running of container

--- judge.sh
+++ judge.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+sub_id="1"
+compilation="/usr/bin/g++ -o2 -static -pipe -o source source.cpp"
+execution="./source < main.in > main.out"
+host_dir="`pwd`""/run"$sub_id
+container_dir="/tmp/run"$sub_id
+commands="cd $container_dir && $compilation && $execution"
 
-SUB_ID="1"
-COMPILATION="/usr/bin/g++ -o2 -static -pipe -o source source.cpp"
-EXECUTION="./source < main.in > main.out"
-DIR_DATA="`pwd`""/run"$SUB_ID
-DIR_RUN="/tmp/run"$SUB_ID
-COMMANDS="cd $DIR_RUN && $COMPILATION && $EXECUTION"
+max_run_time="10"
+max_comp_time="10"
+tl="6"
+ml="250"
 
-docker run -v $DIR_DATA:$DIR_RUN debian-testing sh -c "$COMMANDS"
+container_id=$(docker run -d -v $host_dir:$container_dir debian-testing sh -c "$commands")
+exit_code=$(timeout "$max_run_time" docker wait "$container_id" || true)
+running=$(docker inspect --format="{{ .State.Running }}" "$container_id" 2> /dev/null)
 
+if [ "$running" == "true" ]; then
+  docker kill $cid &> /dev/null
+  echo "Time limit exceeded"
+fi
+
+docker rm -f $container_id &> /dev/null
