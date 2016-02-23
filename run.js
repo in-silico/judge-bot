d1fef30a3622aac09e0ce57e7302ea8fd82a9191
Jhon Jimenez <jhonb3r@gmail.com>
2016-02-23 17:05:52 -0500

    Add support for special judge, resolves #15

--- run.js
+++ run.js
@@ -1,7 +1,6 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
 var data = require('./data.json');
-var exec = require('child_process').exec;
 
 var launch_params = '-m ' + data.memory_limit + 'm -w ' + data.work_directory + ' -v ' +
                      data.volumen + ':' + data.work_directory;
@@ -17,64 +16,85 @@ var launch_container = exec('./launch_container.sh ' + launch_params,
             function(error, stdout, stderr) {
 
               if (error === null) {
-                fs.stat(data.checker, function(err, stat) {
-                  if (err) console.log(err);
-                  if (stat.isFile()) {
-                    var cmp = '/usr/bin/g++ ' + data.checker + '.cpp -o checker';
-                    //exec(cmp)
-                    fs.readdir(data.volumen, function(err, files) {
-                      if (err) {
-                        console.log('error: ', err);
-                        process.exit(4);
-                      }
 
-                      process_submit(0);
+                function check(checker, callback) {
 
-                      function process_submit(i) {
-                        if (i >= files.length) return 0;
-                        else {
-                          if (files[i] && typeof files[i].split == 'function') {
-                            var f = files[i].split('.');
-                            if (f[1] === 'in') {
-                              var execution = data.execution.replace('main.in', files[i]);
-                              execution = execution.replace('main.out', f[0] + '.other');
+                  fs.stat(checker + '.cpp', function(err, stats) {
+                    if (!err && stats.isFile()) {
 
-                              var judge_params = data.time_limit + ' ' + data.memory_limit + ' ' +
-                                container_id + ' ' + f[0] + ' ' + data.checker +
-                                  ' "' + execution + '"';
+                        fs.stat(checker, function(err, stats) {
+                          if (!err && stats.isFile()) {
+                            callback();
+                          }
+                          else {
+                            var cmd = '/usr/bin/g++ ' + checker + '.cpp -o ' + checker;
+                            var cmp = exec(cmd, function(error, stdout, stderr) {
+                              if (error) {
+                                console.log('Error: compilation error of ' + checker + '.cpp');
+                                console.log(stderr);
+                                process.exit(6);
+                              }
+                              callback();
+                            });
+                          }
+                      });
 
-                                  var judge = exec('./judge.sh ' + judge_params,
-                                                   function(error, stdout, stderr) {
+                    }
+                    else {
+                      console.log('Error: file ' + checker + '.cpp not found!');
+                      if (err) console.log(err);
+                      process.exit(5);
+                    }
+                  });
+                }
 
-                                                     if (error === null) {
-                                                       console.log(stdout);
-                                                       process_submit(i + 1);
-                                                     }
-                                                     else {
-                                                       console.log("judge error: ", error);
-                                                       process.exit(3);
-                                                     }
-                                                   });
-                            }
-                            else {
-                              process_submit(i + 1);
-                            }
+                var path_checker = data.volumen + '/' + data.checker;
+                check(path_checker, function() {
+                  fs.readdir(data.volumen, function(err, files) {
+                    if (err) {
+                      console.log('error: ', err);
+                      process.exit(4);
+                    }
+
+                    process_submit(0);
+
+                    function process_submit(i) {
+                      if (i >= files.length) return 0;
+                      else {
+                        if (files[i] && typeof files[i].split == 'function') {
+                          var f = files[i].split('.');
+                          if (f[1] === 'in') {
+                            var execution = data.execution.replace('main.in', files[i]);
+                            execution = execution.replace('main.out', f[0] + '.other');
+
+                            var judge_params = data.time_limit + ' ' + data.memory_limit + ' ' +
+                              container_id + ' ' + f[0] + ' ' + data.checker +
+                              ' "' + execution + '"';
+
+                            var judge = exec('./judge.sh ' + judge_params,
+                                             function(error, stdout, stderr) {
+
+                                               if (error === null) {
+                                                 console.log(stdout);
+                                                 process_submit(i + 1);
+                                               }
+                                               else {
+                                                 console.log("judge error: ", error);
+                                                 process.exit(3);
+                                               }
+                                             });
                           }
                           else {
                             process_submit(i + 1);
                           }
                         }
+                        else {
+                          process_submit(i + 1);
+                        }
                       }
-                    });
-
-                  }
-                  else {
-                    console.log('Error: file ' + data.checker + 'not found!');
-                    process.exit(5);
-                  }
-
+                    }
+                  });
                 });
-
               }
               else {
                 console.log("compile error: ", error);
