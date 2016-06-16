b166e7e03f5e6bb7e99d273fe68b99b0784e206b
Jhon Jimenez <jhonb3r@gmail.com>
2016-06-16 18:25:40 -0500

    Update test_judge.js

--- test/test_judge.js
+++ test/test_judge.js
@@ -4,13 +4,13 @@ var data = {
   _id: "1",
   path: "run1/source",    // path to the submission
   volumen: "run1",        // path to testcases
-  runs: "tmp_runs",       // default to 'data/runs'
+  runs: "tmp_runs",       // path to runs, default to 'data/runs'
   memory_limit: "250",    // maximum allowed memory
   time_limit: "3.5",      // maximum execution time
   compilation: "/usr/bin/g++ -o2 -static -pipe -o Main Main.cpp", // compilation line
-  execution: "./Main < main.in > main.out",                       // execution time
+  execution: "./Main < main.in > main.out",                       // execution line
   extension: ".cpp",      // program extension (cpp, cc, java, etc)
-  checker: "run1/789",         // path to checker
+  checker: "run1/789",    // path to checker
   testcases: [            // array with several test cases in this format
     {
       _id: "1",
